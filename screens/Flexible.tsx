636d614b8edf3c31cc086ad5e2238b1b29eed737
Nguyễn Anh Đức <126655979+ducna-vbee@users.noreply.github.com>
2026-04-21 09:23:54 +0700

    fix: fixed the code for demonstrating flexBasis, flexGrow and flexShrink

--- screens/Flexible.tsx
+++ screens/Flexible.tsx
@@ -1,93 +1,56 @@
+import ScreenDimensionContext from "@/contexts/ScreenDimensionContext";
+import React from "react";
 import { StyleSheet,View } from "react-native";
 
 const Flexible = () => {
+    const {
+        screenWidth,
+        screenHeight,
+    } = React.useContext(ScreenDimensionContext);
+
     return (
-        // <View
-        //     style={{
-        //         flex: 1,
-        //         width: '100%',
-        //         height: '100%',
-        //         display: 'flex',
-        //         flexDirection: 'row',
-        //         alignItems: 'flex-start',
-        //         justifyContent: 'center',
-        //         flexWrap: 'wrap',
-        //         alignContent: 'center',
-        //     }}
-        // >
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        //     <View
-        //         style={{
-        //             ...Styles.viewBox,
-        //             ...{
-        //                 width: 400,
-        //                 height: 200,
-        //             },
-        //         }}
-        //     ></View>
-        // </View>
         <View
             style={{
                 flex: 1,
-                 width: '100%',
-                height: '100%',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
+                width: screenWidth,
+                height: screenHeight,
+                display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'flex-start',
                 justifyContent: 'flex-start',
-                gap: 10,
+                flexWrap: 'wrap',
+                alignContent: 'center',
             }}
         >
-            <View style={[Styles.viewBox,{ width: 150,height: 100 ,flexShrink: 2,}]} />
-            <View style={[Styles.viewBox,{ width: 150,height: 100, flexGrow: 1, }]} />
-            <View style={[Styles.viewBox,{ width: 150,height: 100 ,flexBasis: 150}]} />
-            <View style={[Styles.viewBox,{ width: 150,height: 100 }]} />
-            <View style={[Styles.viewBox,{ width: 150,height: 100 }]} />
-            <View style={[Styles.viewBox,{ width: 150,height: 100 }]} />
+            <View
+                style={{
+                    ...Styles.viewBox,
+                    ...{
+                        width: 300,
+                        height:200,
+                        flexBasis: 400,
+                    },
+                }}
+            ></View>
+            <View
+                style={{
+                    ...Styles.viewBox,
+                    ...{
+                        width: 80,
+                        height: 40,
+                    },
+                }}
+            ></View>
+            <View
+                style={{
+                    ...Styles.viewBox,
+                    ...{
+                        width: 80,
+                        height: 20,
+                        flexGrow: 0.5,
+                    },
+                }}
+            ></View>
         </View>
     );
 };
